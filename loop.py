d6b3bf7a8acb31bb6078ca57b575e57ab2b87444
AnilaAtlas <anila.khan3044@gmail.com>
2026-03-01 23:58:12 +0500

    loop practice

---
+++ loop.py
@@ -0,0 +1,13 @@
+# x = 1
+# while  x < 10:
+#     print("hello:" + str(x))
+#     x = x+1
+
+x = 1 
+while x < 12:
+    print("hello:" + str(x))
+    x = x+1 
+    z = 1
+    while z<10:
+        print("python:" +str(z))
+        z = z+1
