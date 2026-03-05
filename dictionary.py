513e32acc12ad0902e8b5dd99eff73ef99deb9ca
AnilaAtlas <anila.khan3044@gmail.com>
2026-03-05 17:27:33 +0500

    pending

---
+++ dictionary.py
@@ -0,0 +1,28 @@
+student_data = {
+    "name" : "David",
+    "age" : 21,
+    "class" : "10th",
+    "section" : "A",
+    "roll_no" : 1,
+    "marks" : 90,
+    "gender" : "male",
+    "address" : "New York",
+    "phone_no" : "1234567890",
+    "email" : "8t2Uy@example.com",
+    "father_name" : "John",
+    "mother_name" : "Jane"
+}
+
+# #key function
+# student_data = student_data.keys()
+# print(student_data)
+
+#value function
+# student_data = student_data.values()
+# print(student_data)   
+# ----------------- or can use direct this function in print ---------------
+print(student_data.keys())
+
+# copy function
+print(student_data.copy())
+
