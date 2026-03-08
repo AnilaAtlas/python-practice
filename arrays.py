9503c559cc31cca5a2d1f424f254dfb56e0f93cb
AnilaAtlas <anila.khan3044@gmail.com>
2026-03-08 18:01:00 +0500

    done

---
+++ arrays.py
@@ -0,0 +1,15 @@
+from array import *
+arr = array('i', [2,4,5,3,6,7,10,23,25,32])
+print(arr)
+# to find the sum of all arrays
+sumOfarray = sum(arr)
+print(sumOfarray)
+# to find the minimun value in the array
+minimumNum = min(arr)
+print(minimumNum)
+# to find the maximum value in the array
+maxNum = max(arr)
+print(maxNum)
+#calcilate the average of the array elements
+avrg = sum(arr) / len(arr)
+print(avrg)
